9a4609e06e7b7a15de5b4079ecad5e3095ef4da2
Sharareh Akbarian <80437469+ShararAk@users.noreply.github.com>
2021-09-21 17:26:09 +1000

    Add files via upload

--- VisualizeSentinel1.py
+++ VisualizeSentinel1.py
@@ -14,7 +14,7 @@ from stsa import TopsSplitAnalyzer
 parser = argparse.ArgumentParser(description="Visualize Sentinel1")
 
 parser.add_argument('--Image','-I', action="store", type=str, required=True,help="Please indicate the path to downloaded sentinel1 Image")
-parser.add_argument('--Polygon','-P', type=str, required=True, help="Please indicate the path to Shapefile of your area of interest")
+#parser.add_argument('--Polygon','-P', type=str, required=True, help="Please indicate the path to Shapefile of your area of interest")
 
 args =  parser.parse_args(sys.argv[1:])
 
@@ -33,12 +33,13 @@ s1.to_csv('output.csv')
 # The shapefile stored in a geopandas dataframe
 print(s1.df)
 
-# Visualize on a webmap with additional polygon
-#s1.visualize_webmap(polygon='C:/Users/z5235097/OneDrive - UNSW/Skycatch/2. InSAR/Python/Study Area/Kumamoto-polygon.shp')
-
 
 # In[ ]:
 
 
-# python VisualizeSentinel1.py -I="C:/Users/z5235097/OneDrive - UNSW/Skycatch/2. InSAR/Python/S1A_IW_SLC__1SSV_20160408T091355_20160408T091430_010728_01001F_83EB.zip"  -P="C:/Users/z5235097/OneDrive - UNSW/Skycatch/2. InSAR/Python/Study Area/Kumamoto-polygon.shp"
+# Anaconda Prompt
+# cd C:\Users\z5235097\OneDrive - UNSW\Skycatch\2. InSAR\Python
+# python VisualizeSentinel1.py -I="S1A_IW_SLC__1SSV_20160408T091355_20160408T091430_010728_01001F_83EB.zip" 
+# or using stsa.py (https://forum.step.esa.int/t/python-s1-tops-split-analyzer/29047)
+#python stsa.py -zip S1A_IW_SLC__1SSV_20160408T091355_20160408T091430_010728_01001F_83EB.zip --swath iw2 iw3 -polar vv -shp out_shp.shp -csv out_csv.csv -json out_json.jso
 
